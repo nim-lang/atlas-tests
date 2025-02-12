da2c887adcd4d2e67abefc411ae145e2cec7f5a7
Jaremy Creechley <creechley@gmail.com>
2025-02-12 03:25:57 -0700

    setup github actions

--- wsIntegration.nim
+++ wsIntegration.nim
@@ -18,7 +18,7 @@ proc setupWsIntegration() =
           moveFile "hooks"/"post-update.sample", "hooks"/"post-update"
           setFilePermissions("hooks"/"post-update", getFilePermissions("hooks"/"post-update") + {fpUserExec})
           exec "git update-server-info"
-          exec &"echo {name}.git `git rev-parse HEAD` >> ../manifest.txt"
+          exec &"echo {name}.git `git rev-parse HEAD` >> ../../manifest.txt"
 
 proc runWsIntegration*() =
   withDir("atlas-tests"):
